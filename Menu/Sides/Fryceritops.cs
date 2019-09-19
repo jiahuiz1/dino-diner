07dfba1a0e362a1c65012936191782a5ec56b77b
Jiahui Zhao <jiahuiz@users.campus>
2019-09-18 20:47:40 -0500

    add sides

---
+++ Menu/Sides/Fryceritops.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Sides
+{
+    public class Fryceritops : Side
+    {
+        private Size size;
+        public override Size Size
+        {
+            get { return size; }
+
+            set
+            {
+                size = value;
+                switch (size)
+                {
+                    case Size.Small:
+                        Price = 0.99;
+                        Calories = 222;
+                        break;
+                    case Size.Medium:
+                        Price = 1.45;
+                        Calories = 365;
+                        break;
+                    case Size.Large:
+                        Price = 1.95;
+                        Calories = 480;
+                        break;
+                }
+            }
+        }
+
+        public override List<string> Ingredients
+        {
+            get
+            {
+                return this.ingredients;
+            }
+        }
+
+        public Fryceritops()
+        {
+            Price = 0.99;
+            Calories = 222;
+            ingredients.Add("Potato");
+            ingredients.Add("Salt");
+            ingredients.Add("Vegetable Oil");
+        }
+    }
+}
---
+++ Menu/Sides/MeteorMacAndCheese.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Sides
+{
+    public class MeteorMacAndCheese : Side
+    {
+        private Size size;
+
+        public override Size Size
+        {
+            get { return size; }
+            set
+            {
+                size = value;
+                switch (size)
+                {
+                    case Size.Small:
+                        Price = 0.99;
+                        Calories = 420;
+                        break;
+                    case Size.Medium:
+                        Price = 1.45;
+                        Calories = 490;
+                        break;
+                    case Size.Large:
+                        Price = 1.95;
+                        Calories = 520;
+                        break;
+                }
+            }
+        }
+
+        public override List<string> Ingredients
+        {
+            get
+            {
+                return this.ingredients;
+            }
+        }
+
+        public MeteorMacAndCheese()
+        {
+            Price = 0.99;
+            Calories = 420;
+            ingredients.Add("Macaroni Noodles");
+            ingredients.Add("Cheese Product");
+            ingredients.Add("Pork Sausage");
+        }
+    }
+}
